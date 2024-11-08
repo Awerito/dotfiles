c48cc4346b2f05a4508e6994448ad92d6070f4e3
Awerito <diego@openlink.cl>
2024-11-08 16:55:21 -0300

    Fix: slow js formating, npx for local install of prettier

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -520,8 +520,10 @@ require("lazy").setup({
                 htmx = { "prettierd" },
                 html = { "prettierd" },
                 json = { "jq" },
-                js = { "prettierd" },
-                jsx = { "prettierd" },
+                js = { "prettier" },
+                jsx = { "prettier" },
+                ts = { "prettier" },
+                tsx = { "prettier" },
                 css = { "prettierd" },
             },
         },
@@ -758,7 +760,7 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     desc = "Auto-format JS and JSX files after saving",
     callback = function()
         local fileName = vim.api.nvim_buf_get_name(0)
-        vim.cmd(":silent !npx prettier --write " .. fileName)
+        vim.cmd(":silent !prettier --write " .. fileName)
     end,
     group = autocmd_group,
 })
